7f3a3b969d579af2d77821832e254c5c4bf9dafe
Baris <s.barissonmez@gmail.com>
2022-08-14 17:29:17 +0300

    Update cdp-map.py

--- cdp-map.py
+++ cdp-map.py
@@ -35,6 +35,7 @@ def cdp_enable(task):
                                   intf, config_commands=["interface " + intf, "cdp enable"])
         time.sleep(60)  # Wait 60 Seconds to detect new CDP Neighbors
 
+
 def cdp_map(task):
     r = task.run(task=netmiko_send_command,
                  command_string="show cdp neighbor detail", use_genie=True)
@@ -50,3 +51,32 @@ def cdp_map(task):
             "interface " + str(local_intf),
             "description " + str(remote_id)]
         )
+
+
+def cdp_map_po(task):
+    r = task.run(task=netmiko_send_command,
+                 command_string="show etherchannel sum", use_genie=True)
+    task.host["portchannel"] = r.result
+    portchannel = task.host["portchannel"]
+    interfaces = portchannel['interfaces']
+    for poch in interfaces:
+        member_intf = interfaces[poch]['port_channel']['port_channel_member_intfs'][0]
+        des = task.run(task=netmiko_send_command, command_string="show interface {} description".format(
+            member_intf), use_genie=True)
+        member_des = des.result['interfaces'][member_intf]['description']
+        task.run(netmiko_send_config, name="Automating CDP Network Descriptions Port-Channel", config_commands=[
+            "interface " + str(poch),
+            "description " + str(member_des)]
+        )
+
+
+results_enable = nr.run(task=cdp_enable)
+results_map = nr.run(task=cdp_map)
+results_map_po = nr.run(task=cdp_map_po)
+write_mem = nr.run(task=netmiko_send_command,
+                   command_string="write mem", use_genie=True)
+
+print_result(results_enable)
+print_result(results_map)
+print_result(results_map_po)
+print_result(write_mem)
